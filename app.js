d5dbd858b4a381b041486877e6e82a48f8c4ef4a
Hoang Nguyen <duythienguyen1010@gmail.com>
2021-12-20 18:54:55 -0500

    seperating socket and player

--- app.js
+++ app.js
@@ -10,38 +10,39 @@ app.use('/client',express.static(__dirname + '/client'));
  
 serv.listen(2000);
 console.log("Server started.");
- 
+
 var SOCKET_LIST = {};
- 
+var PLAYER_LIST = {};
+var gameMaster ={};
+
+var Player = function(id) {
+	var self = {
+		name:"",
+		role:"",
+		id:id
+	}
+	return self;
+}
+
 var io = require('socket.io')(serv,{});
+
 io.sockets.on('connection', function(socket){
 	socket.role = "";
 	socket.name = "";
 	SOCKET_LIST[socket.id] = socket;
- 
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+
+
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
 	});
- 
+
+socket.on('create_game', function(data){
+	socket.role = data.role;
+	socket.name = data.name;
+})
+
 });
- 
-setInterval(function(){
-	var pack = [];
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.role="wolf";
-		socket.name="tim";
-		pack.push({
-			name:socket.name,
-			role:socket.role,
-		});		
-	}
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.emit('assignRoles',pack);
-	}
- 
- 
- 
- 
-},1000/25);
--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,29 @@
-window.onload = function() {
+ $(document).ready(function(){
     var socket = io();
-        
-    socket.on('assignRoles', function(data){
-        for(var i = 0; i< data.length; i++)
-            document.getElementById("screen").innerHTML = data[i].name + data[i].role;
+    
+    var nameVal = '';
+
+    $("#create_game_btn").click(function(){
+
+        nameVal = $("#name_area").val()
+        if(!isNaN(nameVal) || nameVal === '') {
+            window.alert("Please enter a valid name");
+        }
+        else {
+            socket.emit("create_game", {role:"Game Master", name:nameVal});
+
+        }
     })
-}
+
+    $("#join_game_btn").click(function(){
+
+        nameVal = $("#name_area").val()
+        if(!isNaN(nameVal) || nameVal === '') {
+            window.alert("Please enter a valid name");
+        }
+        else {
+
+        }
+    })
+
+ });
